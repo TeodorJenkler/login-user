1407d7886e0e1f7786e872399eff2dfed0ffc312
Teodor Jenkler <32197435+TeodorJenkler@users.noreply.github.com>
2021-12-02 14:27:47 +0100

    Update customer_update.php

--- customer_update.php
+++ customer_update.php
@@ -1,1 +1,28 @@
+<?php 
 
+// Inkludera filer för databas och funktioner
+require("includes/conn_mysql.php");
+require("includes/customer_functions.php")
+  
+  // Skapa databas
+$connection = dbConnect();
+
+// Ska kund ändras?
+
+if(isset($_GET['editid']) && $_GET['editid']>0){
+  $customerData = 
+}
+?>
+
+<!DOCTYPE HTML>
+<html lang='sv'>
+   <head>
+      <meta charset="utf-8"/>
+   </head>
+   <body>
+     <h1>Updattera kund</h1>
+     
+     <form action="customer_update.php" mehtod="post">
+        <input type="hidden name="updateid" value="<?php echo $customerData['customerID']; ?>">
+        
+   </body>
