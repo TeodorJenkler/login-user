0add5670407f1f84440c62cb244bd804f1119349
Teodor Jenkler <32197435+TeodorJenkler@users.noreply.github.com>
2021-12-05 19:54:07 +0100

    Create logout.php

---
+++ logout.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+
+unset($_SESSION['status']);
+
+session_destroy();
+
+?>
+
+<!DOCTYPE HTML>
+<html lang="sv">
+  <head>
+    <meta charset="utf-8"/>
+    <title>Kunder Logga In</title>
+  </head>
+  <body>
+    <form action="checklogin.php" method="post">
+      <h1>Logga In</h1>
+      <label>E-post:</label>
+      <p><input type="email" name="txtUser"></p>
+      <label>Lösenord:</label>
+      <p><input type="password" name="txtPassword"></p>
+      <p><input type="submit" value="Logga in"></p>
+    </form>
+  </body>
+</html>
