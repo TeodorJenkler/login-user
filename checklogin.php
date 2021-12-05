499a71443e66b33eea717fa5fa425260dcb83ff3
Teodor Jenkler <32197435+TeodorJenkler@users.noreply.github.com>
2021-12-05 19:43:54 +0100

    Create checklogin.php

---
+++ checklogin.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+?>
+  <?php
+    require("includes/conn_mysql.php");
+  
+    $connection = dbConnect();
+    $checkUser =  mysqli_real_escape_string($conn, $_POST['txtUser']);
+    $checkPassword = mysqli_real_escape_string($conn, $_POST['txtPassword']);
+  
+    $query = "SELECT * FROM customer
+                WHERE customerEmail = '$checkUser';
+         
+    $result = mysqli_query($connection, $query) Or die("Query failed: $query");
+    
+    $row = mysqli_fetch_assoc($result);
+    
+    $count = mysqli_num_rows($result);
+    
+    if(count == 1){
+      if(password_hash($checkPassword, $row["customerPassword"])){
+        $_SESSION['status'] = "ok";
+        header("Location: customer_read.php");
+      }
+    }
+     header("Location: index.php");
+    
+    mysqli_close($connection);
+  ?>
